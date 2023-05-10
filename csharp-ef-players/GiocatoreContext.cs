637bd60c95223033624d2a73a5a65acb4c9490ea
Khodazone <khodazone@gmail.com>
2023-05-10 18:17:56 +0200

    1 commit

--- csharp-ef-players/GiocatoreContext.cs
+++ csharp-ef-players/GiocatoreContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace csharp_ef_players
 {
-    public class GiocatoreContext:DBContext
+    public class GiocatoreContext : DbContext
     {
+        public DbSet<Giocatore> Giocatore { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EfGiocatore;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
     }
 }
---
+++ csharp-ef-players/Migrations/20230510153044_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharp_ef_players.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Giocatore",
+                columns: table => new
+                {
+                    GiocatoreId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cognome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Punteggio = table.Column<double>(type: "float", nullable: false),
+                    PartiteGiocate = table.Column<int>(type: "int", nullable: false),
+                    PartiteVinte = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Giocatore", x => x.GiocatoreId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Giocatore");
+        }
+    }
+}
