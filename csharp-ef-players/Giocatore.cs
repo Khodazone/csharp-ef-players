507017af26394f8c0eb0c95fff110234d75adf93
Khodazone <khodazone@gmail.com>
2023-05-10 16:53:46 +0200

    context

--- csharp-ef-players/Giocatore.cs
+++ csharp-ef-players/Giocatore.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace csharp_ef_players
 {
-    public class Player
+    public class Giocatore
     {
         [Key]
-        public int PlayerId { get; set; }
+        public int GiocatoreId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public double Punteggio { get; set; }
+
+        public int PartiteGiocate { get; set; }
+
+        public int PartiteVinte { get; set; }
     }
 }
+
+
